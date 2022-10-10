efe52b5583fd87ff1bb003c2ffb0a38c711104f5
fluxfluxtv <izotych@list.ru>
2022-10-10 18:01:45 +0300

    initial commit

---
+++ MathOperation/MathFunc.cs
@@ -0,0 +1,24 @@
+namespace ConsoleApp1;
+
+public class MathFunc
+{
+    public static double Plus(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Sub(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Mult(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Div(double a, double b)
+    {
+        return a / b;
+    }
+}
---
+++ MathOperation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? exit = "";
+            while (exit != "!quit")
+            {
+                Console.WriteLine("Введите a и b");
+
+                double a = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите операцию");
+                var func = Console.ReadLine();
+                bool operationSet = true;
+                double result = OperationResult(func, a, b, ref operationSet);
+
+                if (operationSet)
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Пошёл нахуй");
+                }
+
+                Console.WriteLine("Хочешь выйти пидр, если да то введи !quit");
+                exit = Console.ReadLine();
+            }
+        }
+
+        private static double OperationResult(string? operation, double a, double b, ref bool operationSet)
+        {
+            double resultOp = 0;
+            switch (operation)
+            {
+                case "/":
+                    resultOp = MathFunc.Div(a, b);
+                    break;
+                case "+":
+                    resultOp = MathFunc.Plus(a, b);
+                    break;
+                case "-":
+                    resultOp = MathFunc.Sub(a, b);
+                    break;
+                case "*":
+                    resultOp = MathFunc.Mult(a, b);
+                    break;
+                default:
+                    operationSet = false;
+                    break;
+            }
+
+            return resultOp;
+        }
+    }
+}
